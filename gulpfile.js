7c45e5535e48a395896d7b401d26f39ac5b11f6e
Edison Lamar <edilata@msn.com>
2020-02-19 23:59:03 +0100

    Adding the loop

--- gulpfile.js
+++ gulpfile.js
@@ -14,15 +14,15 @@ sass.compiler = require('node-sass');
 // Compilar los ficheros SCSS y enviar el CSS resutlante
 // en la carpeta /assets/css
 gulp.task('sass', function () {
-  return gulp.src('./scss/*.scss')
+  return gulp.src('./sass/**/*.scss')
   .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'));
+    .pipe(sourcemaps.write('../../sass/sourcemap/'))
+    .pipe(gulp.dest('./assets/css'));
 });
 
 // Vigilar si hay algún cambio en los SCSS
 gulp.task('default', function () {
-  gulp.watch('./scss/*.scss', gulp.series('sass'));
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
 
